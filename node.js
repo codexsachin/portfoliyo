804c7fedef51e97545d3370c2bd402fe40a81e0c
suchi092 <mkp9424009364@gmail.com>
2023-01-29 17:46:27 +0530

    first commit

---
+++ node.js
@@ -0,0 +1,4 @@
+// a = 5;
+// b = 10;
+// console.log(a + b)
+// we can tasks here for lopp
---
+++ script2.js
@@ -0,0 +1,6 @@
+!
+sachin @sachin opande ki clas me ham jayrenge lekin ak shart pe because we will dont aboooouurtt discoue devlopmengtme ham ki
+;
+hjw
+ki
+PublicKeyCredentialk discouebhjbn
